67d012f908e67bda1120f63c0203fb0c414b73c8
ArianaMendonza03 <mendozaariana954@gmail.com>
2021-11-21 21:04:15 +1200

    Agregar archivos de proyecto.

---
+++ ArianaProgramación/ContarPalabras.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ArianaProgramación
+{
+    public partial class ContarPalabras : Form
+    {
+        public ContarPalabras()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int conteo = 0, i = 0;
+            string frase = textBox1.Text;
+            while (frase[i] == ' ') 
+                i++;
+            if (frase[i] == '.') 
+                conteo--;
+            while (frase[i] != '.')
+            {
+                if (frase[i] == ' ')
+                {
+                    conteo++;
+                    while (frase[i] == ' ') i++;
+                    if (frase[i] == '.') conteo++;
+                }
+                else
+                {
+                    i++;
+                }
+            }
+            conteo++;
+            Console.WriteLine($"{0}", conteo);
+            this.textBox2.Text = conteo.ToString();
+
+        }
+    }
+}
---
+++ ArianaProgramación/RevezPalabras.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ArianaProgramación
+{
+    public partial class RevezPalabras : Form
+    {
+        public RevezPalabras()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string Entrada = textBox1.Text;
+            string Salida = "";
+            for (int x = Entrada.Length - 1; x >= 0; x--)
+            {
+                Salida += Entrada[x];
+            }
+            Console.WriteLine($"{0}", Salida);
+            this.textBox2.Text = Salida.ToString();
+
+        }
+    }
+}
